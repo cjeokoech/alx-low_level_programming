abaeffe7b12ebd80c44c76a00a27616ffa048915
caroline <komencaroline2@gmail.com>
2023-07-17 07:25:28 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Print numbers
+*
+* Description: The program description
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+return (0);
+}

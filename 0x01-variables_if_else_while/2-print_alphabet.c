f28359194a8b489f05bff90445aae24abacd7ace
caroline <komencaroline2@gmail.com>
2023-07-17 06:34:10 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: The program description
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i <= 26; i++)
+{
+putchar(alp[i]);
+}
+return (0);
+}

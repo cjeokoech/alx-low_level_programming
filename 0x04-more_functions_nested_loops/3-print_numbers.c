350f68068f4d6c0274aad8a9b1fe5b8c4cddfd67
caroline <komencaroline2@gmail.com>
2023-07-22 09:53:04 -0700

    ./3-print_numbers | cat -e

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - to print numbers
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

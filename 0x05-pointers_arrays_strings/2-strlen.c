c5d21893ddf98cb979ff1a939be8073067407d65
caroline <komencaroline2@gmail.com>
2023-07-24 06:10:53 -0700

    ./2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - return lenght of string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (i != '0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}

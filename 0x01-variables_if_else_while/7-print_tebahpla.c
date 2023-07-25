31b8f50cf876ac87726452c6ba56000b6b04ac3b
caroline <komencaroline2@gmail.com>
2023-07-25 01:27:03 -0700

    ./9-print_comb

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,12 +8,11 @@
 */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char ch;
 
-	for (i = 0; i < 26; i--)
+	for (ch = z; ch >= a; ch--)
 	{
-		putchar(alp[i]);
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);

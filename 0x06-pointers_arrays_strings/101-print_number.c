e22ee9d319991274cacdd36a77f5e62689e27df1
caroline <komencaroline2@gmail.com>
2023-07-26 09:32:38 -0700

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - print integer
+ * @n: input integer
+ * Return: n
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	i = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+	if (i / 10 != 0)
+	{
+		print_number(i / 10);
+	}
+	_putchar((i % 10) + '0');
+	_putchar('\n');
+}

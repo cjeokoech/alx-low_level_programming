953ba687c10f5eb041b7c9c4392c0a1063d8bb8f
caroline <komencaroline2@gmail.com>
2023-07-26 04:50:08 -0700

    ./6-cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * cap_string - capitilize all words of a string
+ * @n: pointer
+ * Return: n
+ */
+char *cap_string(char *n)
+{
+	int i = 0;
+
+	while (n[i])
+	{
+		while (!(n[i] >= 'a' && n[i] <= 'z'))
+			i++;
+
+		if (n[i - 1] == ' ' ||
+				n[i - 1] == '\t' ||
+				n[i - 1] == '\n' ||
+				n[i - 1] == ',' ||
+				n[i - 1] == ';' ||
+				n[i - 1] == '_' ||
+				n[i - 1] == '!' ||
+				n[i - 1] == '?' ||
+				n[i - 1] == '"' ||
+				n[i - 1] == '(' ||
+				n[i - 1] == ')' ||
+				n[i - 1] == '{' ||
+				n[i - 1] == '}' ||
+				i == 0)
+			n[i] -= 32;
+		i++;
+	}
+	return (n);
+}

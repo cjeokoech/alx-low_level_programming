f27cfc59d8b5c483e510cbca6b0603dc649bc985
caroline <komencaroline2@gmail.com>
2023-08-08 02:26:30 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - to print its name
+ * @argc: argument count
+ * @argv: array pointer
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

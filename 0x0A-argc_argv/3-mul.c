423b15e83d338d5a1256ecaddf05a63107057b3f
caroline <komencaroline2@gmail.com>
2023-08-08 03:08:29 -0700

    ./mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - print multiplies of two numbers
+ * @argc: the number of command line arg
+ * @argv: the array of command line arg
+ * Return: 0 on success or 1 on Error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}

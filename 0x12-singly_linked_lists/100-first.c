68d1bf577a076540b12f9597409de3e56144062d
caroline <komencaroline2@gmail.com>
2023-08-24 03:36:33 -0700

    update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+void first(void) __attribute__ ((constructor));
+/**
+ *first - print the following words
+ * Return: 0
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

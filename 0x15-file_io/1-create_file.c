94e63b534771ac08044c805c8f40b67fa2560dbc
cjeokoech <cjeokoech@gmail.com>
2023-10-02 07:41:04 -0700

    update

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_file - create file
+ * @filename: name of the file to create
+ * @text_content: text contained in filename
+ * Return: 1 on success or -1 on fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t write_fd;
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	write_fd = write(fd, text_content, strlen(text_content));
+	if (write_fd == -1 || fd == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}

a79f8cbd76bbe8cc590b033a5f60ad6b70f6941d
cjeokoech <cjeokoech@gmail.com>
2023-10-02 09:32:34 -0700

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,15 +7,20 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, len = 0;
 	ssize_t write_fd;
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	write_fd = write(fd, text_content, strlen(text_content));
+	write_fd = write(fd, text_content, len);
 	if (write_fd == -1)
 		return (-1);
 	close(fd);

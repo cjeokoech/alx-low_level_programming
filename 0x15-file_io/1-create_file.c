259939d4e69460c9dc849a4dd56ee79a581f312d
cjeokoech <cjeokoech@gmail.com>
2023-10-02 09:28:59 -0700

    updte

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,14 +7,19 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, len = 0;
 	ssize_t write_fd;
 
 	if (filename == NULL)
 		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	write_fd = write(fd, text_content, strlen(text_content));
+	write_fd = write(fd, text_content, len);
 	if (write_fd == -1 || fd == -1)
 	{
 		return (-1);

46283ece830052e72ecec49a57eac1dac4f60ad4
caroline <cjeokoech@gmail.com>
2023-12-11 07:39:17 -0800

    update

---
+++ 0x18-dynamic_libraries/cal.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+/**
+ * add - add two number
+ * @a: number
+ * @b: number 
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two numbers
+ * @a: number
+ * @b: number
+ * Return: subtraction of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+ /**
+  * mul - multiplication of two numbers
+  * @a: number
+  * @b: number
+  * Return: multiplication of a and b
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * mod - mudula of two numbers
+ * @a: number
+ * @b: number
+ * Return: mudula
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+/**
+ * divi - diviaion of two numbers
+ * @a: number
+ * @b: number
+ * Return: division of two numbers
+ */
+int divi(int a, int b)
+{
+	return (a / b);
+}

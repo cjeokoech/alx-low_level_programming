f71502ed6b10e2135862621960f8b90649c5d8d0
cjeokoech <cjeokoech@gmail.com>
2023-12-22 08:43:23 -0800

    update

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,46 +1,26 @@
 #include "hash_tables.h"
 
-void delete_node(hash_node_t *);
 /**
- * hash_table_delete - delete a hash table.
- * @ht: hash table to delete.
- *
+ * hash_table_delete - deletes a hash table.
+ * @ht: the hash table.
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i = 0;
 	hash_node_t *node;
-	unsigned long int idx = 0;
 
-	while (idx < ht->size)
+	while (i < ht->size)
 	{
-		if (ht->array[idx])
+		while (ht->array[i] != NULL)
 		{
-			node = ht->array[idx];
-			delete_node(node);
+			node = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = node;
 		}
-		idx++;
+		i++;
 	}
 	free(ht->array);
 	free(ht);
 }
-
-/**
- * delete_node - deletes a bucket
- * @node: nodes to delete
- */
-void delete_node(hash_node_t *node)
-{
-	hash_node_t *tmp;
-
-	while (node)
-	{
-		tmp = node;
-		free(node->key);
-		if (node->value)
-			free(node->value);
-		node = node->next;
-		free(tmp);
-	}
-}
-
-
